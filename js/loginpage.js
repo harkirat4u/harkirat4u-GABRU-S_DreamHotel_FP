87032d75b743b51e1c6cfb2f6ff574acc04f168e
harkirat singh <singhharkirat051@gmail.com>
2020-06-06 09:40:07 -0400

    change version of database

--- js/loginpage.js
+++ js/loginpage.js
@@ -1,5 +1,4 @@
-
-window.indexedDB = window.indexedDB|| window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
+window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
 
 const list1 = document.querySelector('#ul1');
 const user1 = document.querySelector('#user');
@@ -16,7 +15,7 @@ const submitBtn1 = document.querySelector('form button');
 
 
 let db;
-const request = window.indexedDB.open('Registration', 1);
+const request = window.indexedDB.open('Registration', 4);
 console.log('Database enter');
 window.onload = function() {
     // let request = window.indexedDB.open('Registration', 1);
@@ -24,28 +23,28 @@ window.onload = function() {
 
     request.onerror = function() {
         console.log('Database failed to open');
-      };
+    };
 
-      request.onsuccess = function() {
+    request.onsuccess = function() {
         console.log('Database opened successfully');
-      
+
         db = request.result;
- 
+
         // Run the displayData() function to display the notes already in the IDB
         //displayData();
         //createbtn();
-      }
+    }
 
-      form1.onsubmit = loginfunction;
+    form1.onsubmit = loginfunction;
 
 
-      //Login fn
+    //Login fn
 
-      function loginfunction(e){
+    function loginfunction(e) {
         e.preventDefault();
-        let userinput=user.value;
-        const request = window.indexedDB.open("Registration", 1);
-       
+        let userinput = user.value;
+        const request = window.indexedDB.open("Registration", 4);
+
         request.onsuccess = () => {
             const db = request.result;
             const transaction = db.transaction(['Login'], 'readwrite');
@@ -55,63 +54,62 @@ window.onload = function() {
                 const cursor = e.target.result;
                 if (cursor) {
                     if (cursor.value.user === userinput) {
-                        alert("Welcome:"+cursor.value.user);
-                      // createbtn();
+                        alert("Welcome:" + cursor.value.user);
+                        // createbtn();
 
-                      displayData();
-                      console.log('logined');
-                      console.log('logined'+cursor.value.user);
-                    //  cursor.preventDefault;
+                        displayData();
+                        console.log('logined');
+                        console.log('logined' + cursor.value.user);
+                        //  cursor.preventDefault;
                     }
                     // else{
                     //     alert("User Invalid,Please register first")
                     // }
                     cursor.continue();
                     console.log('continue');
-                }
-                else{
+                } else {
                     console.log('login-else');
                     //alert("Invalid User/ Please Register first");
                 }
-               
+
             }
             transaction.oncomplete = function() {
                 console.log('Transaction completed: database modification finished.');
-            
+
                 // update the display of data to show the newly added item, by running displayData() again.
-               // displayData();
-              };
-            
-              transaction.onerror = function() {
+                // displayData();
+            };
+
+            transaction.onerror = function() {
                 console.log('Transaction not opened due to error');
-              };
+            };
         }
 
-     
-
-}
 
 
-function displayData() {
-    
-  
-    // const request = window.indexedDB.open("Registration", 1);
-    // // Here we empty the contents of the list element each time the display is updated
-    // // If you didn't do this, you'd get duplicates listed each time a new note is added
-    while (list1.firstChild) {
-      list1.removeChild(list.firstChild);
     }
-  
-    // // Open our object store and then get a cursor - which iterates through all the
-    // // different data items in the store
-    // let objectStore = db.transaction('Login').objectStore('Login');
-    // objectStore.openCursor().onsuccess = function(e) {
-    //   // Get a reference to the cursor
-    //   let cursor = e.target.result;
-    //   request.onsuccess = () => {
-       
-        const username=user.value;
-    //   alert(username);
+
+
+    function displayData() {
+
+
+        // const request = window.indexedDB.open("Registration", 1);
+        // // Here we empty the contents of the list element each time the display is updated
+        // // If you didn't do this, you'd get duplicates listed each time a new note is added
+        while (list1.firstChild) {
+            list1.removeChild(list.firstChild);
+        }
+
+        // // Open our object store and then get a cursor - which iterates through all the
+        // // different data items in the store
+        // let objectStore = db.transaction('Login').objectStore('Login');
+        // objectStore.openCursor().onsuccess = function(e) {
+        //   // Get a reference to the cursor
+        //   let cursor = e.target.result;
+        //   request.onsuccess = () => {
+
+        const username = user.value;
+        //   alert(username);
         const db = request.result;
         const transaction = db.transaction(['Login'], 'readwrite');
         const invStore = transaction.objectStore('Login');
@@ -120,7 +118,7 @@ function displayData() {
         const cursorRequest = vendorIndex.openCursor(keyRng);
         cursorRequest.onsuccess = e => {
 
-           
+
             const cursor = e.target.result;
             if (cursor) {
                 // alert(cursor.value.user);
@@ -132,41 +130,38 @@ function displayData() {
                 const para = document.createElement('p');
                 const para1 = document.createElement('p');
                 const para2 = document.createElement('p');
-                const para3= document.createElement('p');
-              
-          
+                const para3 = document.createElement('p');
+
+
                 listItem.appendChild(h3);
                 listItem.appendChild(para);
                 listItem.appendChild(para1);
                 listItem.appendChild(para2);
                 listItem.appendChild(para3);
-               
+
                 list1.appendChild(listItem);
-          
+
                 // Put the data from the cursor inside the h3 and para
                 h3.textContent = cursor.value.user;
                 para.textContent = cursor.value.lastname;
                 para1.textContent = cursor.value.email;
                 para2.textContent = cursor.value.phone;
                 para3.textContent = cursor.value.sex;
-              
-    
+
+
                 cursor.continue();
+            } else {
+                // Again, if list item is empty, display a 'No notes stored' message
+                if (!list1.firstChild) {
+                    const listItem = document.createElement('li');
+                    listItem.textContent = 'No notes stored.';
+                    list.appendChild(listItem);
+                }
+                // if there are no more cursor items to iterate through, say so
+                console.log('Notes all displayed');
             }
-        
-
-      else {
-        // Again, if list item is empty, display a 'No notes stored' message
-        if(!list1.firstChild) {
-          const listItem = document.createElement('li');
-          listItem.textContent = 'No notes stored.';
-          list.appendChild(listItem);
         }
-        // if there are no more cursor items to iterate through, say so
-        console.log('Notes all displayed');
-      }
-    }
-    
-  }
 
-    };
+    }
+
+};
--- js/new.js
+++ js/new.js
@@ -12,7 +12,7 @@ const form = document.querySelector('#regform');
 const submitBtn = document.querySelector('form button');
 let db;
 window.onload = function() {
-    let request = window.indexedDB.open('Registration', 3);
+    let request = window.indexedDB.open('Registration', 4);
 
 
     request.onerror = function() {
@@ -47,7 +47,7 @@ window.onload = function() {
         objectStore.createIndex('room', 'room', { unique: false });
         objectStore.createIndex('person', 'person', { unique: false });
         objectStore.createIndex('other', 'other', { unique: false });
-        
+
 
         console.log('Database setup complete');
     };
@@ -61,7 +61,7 @@ window.onload = function() {
         e.preventDefault();
 
         // grab the values entered into the form fields and store them in an object ready for being inserted into the DB
-        let newItem = { user: Fname.value, lastname: Lname.value, email: email.value, phone: phone.value, bname: '', ndays: '', room:'',person:'',other:'',};
+        let newItem = { user: Fname.value, lastname: Lname.value, email: email.value, phone: phone.value, bname: '', ndays: '', room: '', person: '', other: '', };
 
         // open a read/write db transaction, ready for adding the data
         let transaction = db.transaction(['Login'], 'readwrite');
@@ -77,7 +77,7 @@ window.onload = function() {
             Lname.value = '';
             email.value = '';
             phone.value = '';
-         
+
         };
 
         // Report on the success of the transaction completing, when everything is done
@@ -97,21 +97,21 @@ window.onload = function() {
 
 
     function displayData() {
-    
-  
+
+
         // const request = window.indexedDB.open("Registration", 1);
         // // Here we empty the contents of the list element each time the display is updated
         // // If you didn't do this, you'd get duplicates listed each time a new note is added
         while (list.firstChild) {
-          list.removeChild(list.firstChild);
+            list.removeChild(list.firstChild);
         }
-      
-       
-        const request = window.indexedDB.open("Registration", 3);
-      request.onsuccess = () => {
-           
-            let username1=user.value;
-             alert(username);
+
+
+        const request = window.indexedDB.open("Registration", 4);
+        request.onsuccess = () => {
+
+            let username1 = user.value;
+            alert(username);
             const db = request.result;
             const transaction = db.transaction(['Login'], 'readwrite');
             const invStore = transaction.objectStore('Login');
@@ -119,8 +119,8 @@ window.onload = function() {
             const keyRng = IDBKeyRange.only(username1);
             const cursorRequest = vendorIndex.openCursor(keyRng);
             cursorRequest.onsuccess = e => {
-    
-               
+
+
                 const cursor = e.target.result;
                 if (cursor) {
                     // alert(cursor.value.user);
@@ -132,10 +132,10 @@ window.onload = function() {
                     const para = document.createElement('p');
                     const para1 = document.createElement('p');
                     const para2 = document.createElement('p');
-                    const para3= document.createElement('p');
-                    const para4= document.createElement('p');
-                  
-              
+                    const para3 = document.createElement('p');
+                    const para4 = document.createElement('p');
+
+
                     listItem.appendChild(h3);
                     // listItem.appendChild(para);
                     // listItem.appendChild(para1);
@@ -143,30 +143,27 @@ window.onload = function() {
                     listItem.appendChild(para3);
                     listItem.appendChild(para4);
                     list1.appendChild(listItem);
-              
+
                     // Put the data from the cursor inside the h3 and para
-                    h3.textContent = "Welcome-:"+cursor.value.user;
-                    para3.textContent = "Lastname-"+cursor.value.lastname;
-                    para2.textContent = "Phone-"+cursor.value.email;
-                    para4.textContent = "Phone-"+cursor.value.phone;
-        
+                    h3.textContent = "Welcome-:" + cursor.value.user;
+                    para3.textContent = "Lastname-" + cursor.value.lastname;
+                    para2.textContent = "Phone-" + cursor.value.email;
+                    para4.textContent = "Phone-" + cursor.value.phone;
+
                     cursor.continue();
+                } else {
+                    // Again, if list item is empty, display a 'No notes stored' message
+                    if (!list.firstChild) {
+                        const listItem = document.createElement('li');
+                        listItem.textContent = 'No notes stored.';
+                        list.appendChild(listItem);
+                    }
+                    // if there are no more cursor items to iterate through, say so
+                    console.log('Notes all displayed');
                 }
-            
-    
-          else {
-            // Again, if list item is empty, display a 'No notes stored' message
-            if(!list.firstChild) {
-              const listItem = document.createElement('li');
-              listItem.textContent = 'No notes stored.';
-              list.appendChild(listItem);
             }
-            // if there are no more cursor items to iterate through, say so
-            console.log('Notes all displayed');
-          }
+
         }
-        
-      }
-    
-}
+
+    }
 };
